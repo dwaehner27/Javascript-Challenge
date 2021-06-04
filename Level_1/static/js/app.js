a365c9ce42f12bf0e7446fed9062318cf5fa5f0c
Dan Waehner <dwaehner97@gmail.com>
2021-06-03 19:21:24 -0500

    updated

--- Level_1/static/js/app.js
+++ Level_1/static/js/app.js
@@ -1,4 +1,41 @@
-// from data.js
-var tableData = data;
+const tableData = data;
 
-// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+function buildOriginalTable() {
+  data.forEach(originalData =>  {
+    var row = tbody.append("tr");
+    Object.entries(originalData).forEach(([_, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });  
+  });
+};
+
+buildOriginalTable();
+
+var button = d3.select("#filter-btn");
+
+button.on("click",function() {
+    
+    d3.event.preventDefault();
+    tbody.html("");
+  
+    var UserInput = d3.select("#datetime");
+    var InputVal = UserInput.property("value");
+
+    if (InputVal == "") {
+      buildOriginalTable();
+      return;
+    }
+    
+    var filteredData = data.filter(data => data.datetime === InputVal);
+  
+    filteredData.forEach(newData => {
+      var row = tbody.append("tr");
+      Object.entries(newData).forEach(([_, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+      });
+    });
+});
--- Level_2/static/js/app.js
+++ Level_2/static/js/app.js
@@ -1,4 +1,60 @@
-// from data.js
-var tableData = data;
+const tableData = data;
 
-// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+function buildOriginalTable() {
+  data.forEach(originalData =>  {
+    var row = tbody.append("tr");
+    Object.entries(originalData).forEach(([_, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });  
+  });
+};
+
+buildOriginalTable();
+
+var button = d3.select("#filter-btn");
+
+button.on("click",function() {
+    
+    d3.event.preventDefault();
+    tbody.html("");
+  
+    var DateInput = d3.select("#datetime").property("value");
+    var CityInput = d3.select("#city").property("value");
+    var StateInput = d3.select("#state").property("value");
+    var CountryInput = d3.select("#country").property("value");
+    var ShapeInput = d3.select("#shape").property("value");
+
+    if (DateInput == "" && CityInput == "" && StateInput == "" && CountryInput == "" && ShapeInput == "") {
+      buildOriginalTable();
+      return;
+    }
+    
+    var filteredData = data;
+
+    if(DateInput) {
+      filteredData = filteredData.filter(data => data.datetime === DateInput);
+    }
+    if(CityInput) {
+      filteredData = filteredData.filter(data => data.city === CityInput);
+    }
+    if(StateInput) {
+      filteredData = filteredData.filter(data => data.state === StateInput);
+    }
+    if(CountryInput) {
+      filteredData = filteredData.filter(data => data.country === CountryInput);
+    }
+    if(ShapeInput) {
+      filteredData = filteredData.filter(data => data.shape === ShapeInput);
+    }
+
+    filteredData.forEach(newData => {
+      var row = tbody.append("tr");
+      Object.entries(newData).forEach(([_, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+      });
+    });
+});
